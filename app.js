20fdf727cf2fed6a2d9aaaa066a44e81ed657a8f
Ola Frick <info@culturehero.org>
2017-10-03 14:28:37 +0200

    Adding a simple "logger" middleware

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-// Starting Over
+// Setting up Express
+const express = require('express');
+const app = express();
+const host = 'http://localhost';
+const port = '3000';
 
+// Adding Middleware
+app.use(myLogger);
+
+app.use(express.static('./www'));
+
+app.listen(port, ()=> console.log(`Listning on Port ${port}`));
+
+function myLogger(req, res, next) {
+    console.log(`${req.hostname}:${port}${req.url}`);
+    next();
+}
+
+function myNextLogger(req, res, next) {
+    if(req.url === '/test'){
+        res.end('Whowa Wiiwa');
+    } else {
+        next();
+    }
+}
